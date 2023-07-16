abb21834138725ac6b16a81705bc385225be070e
CESAR NUNES DE OLIVEIRA <ra22139193-5@aluno.unicesumar.edu.br>
2023-07-16 01:26:56 -0300

    CleanedUp the Code and added Modal

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const resetarJogo = document.querySelector('.reset');
 const btnSortear = document.querySelectorAll('.sortear');
 const estadoJogo = document.querySelector('.estadoJogo');
@@ -5,6 +7,7 @@ const elementoSorteado = document.querySelector('.elementoSorteado');
 
 let numerosSorteados = [];
 let maxNumber = 118;
+
 const resetar = function() {
     const numeros = document.querySelectorAll('.numero');
     numeros.forEach(numero => numero.classList.remove('sorteado'));
@@ -16,12 +19,13 @@ const resetar = function() {
 resetarJogo.addEventListener('click', resetar);
 
 const sortear = function() {
-let numero = generateUniqueNumber();
+    let numero = generateUniqueNumber();
 
     if (numerosSorteados.includes(numero)) {
         return;
     } else {
         let letter = '';
+
         if ([1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76, 81, 86, 91, 96, 101, 106, 111, 116].includes(numero)) {
             letter = 'B';
         } else if ([2, 7, 12, 17, 22, 27, 32, 37, 42, 47, 52, 57, 62, 67, 72, 77, 82, 87, 92, 97, 102, 107, 112, 117].includes(numero)) {
@@ -33,16 +37,17 @@ let numero = generateUniqueNumber();
         } else if ([5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120].includes(numero)) {
             letter = 'O';
         }
+
         if (letter === undefined) {
             return;
-
         }
 
         numerosSorteados.push(numero);
-        let elemento = elementos_quimicos[numero -1];
+        let elemento = elementos_quimicos[numero - 1];
         estadoJogo.textContent = `${letter} - ${numero}`;
         const numeroSorteado = document.querySelector(`.numero.num-${numero}`);
         elementoSorteado.textContent = `${elemento}`;
+
         if (numeroSorteado) {
             numeroSorteado.classList.add('sorteado');
         }
@@ -51,13 +56,43 @@ let numero = generateUniqueNumber();
 
 const generateUniqueNumber = function() {
     let numero;
+
     do {
-      numero = Math.floor(Math.random() * maxNumber) + 1;
+        numero = Math.floor(Math.random() * maxNumber) + 1;
     } while (numerosSorteados.includes(numero));
+
     return numero;
-  };
+};
 
 btnSortear.forEach(btn => btn.addEventListener('click', sortear));
+
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 const elementos_quimicos = [
     "Hidrogênio",
